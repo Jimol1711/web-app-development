d646c780dde13db5e70e195a7af937eab5ca3440
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-10-16 20:47:15 -0300

    Ahora sí

--- TAREAS/T2/VENV_copy/app.py
+++ TAREAS/T2/VENV_copy/app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("templates/index.html")
+
+@app.route("/register_artesano", methods=["GET", "POST"])
+def register():
--- TAREAS/T2/VENV_copy/db/db.py
+++ TAREAS/T2/VENV_copy/db/db.py
@@ -1,7 +1,23 @@
 import pymysql
+import json
+
+DB_NAME = "tarea2"
+DB_USERNAME = "cc5002"
+DB_PASSWORD = "programacionweb"
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_CHARSET = "utf8"
+
+with open('database/querys.json', 'r') as querys:
+	QUERY_DICT = json.load(querys)
 
 def getArtesano():
-    artesano = pymysql.connect(
-        db = 'artesano_db'
-        
-    )
+	conn = pymysql.connect(
+		db=DB_NAME,
+		user=DB_USERNAME,
+		passwd=DB_PASSWORD,
+		host=DB_HOST,
+		port=DB_PORT,
+		charset=DB_CHARSET
+	)
+	return conn
